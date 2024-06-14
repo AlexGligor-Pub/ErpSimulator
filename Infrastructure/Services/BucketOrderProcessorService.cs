b3483f7b3c6d31cef99b79e8c947994e25bb6504
DicuMariaF <maria.f.dicu@accenture.com>
2024-06-14 11:53:35 +0300

    sent to UNS

--- Infrastructure/Services/BucketOrderProcessorService.cs
+++ Infrastructure/Services/BucketOrderProcessorService.cs
@@ -1,10 +1,13 @@
 ﻿using Domain.Entities.UNS;
+using HiveMQtt.Client.Options;
 using System.Text.Json;
 
 namespace Infrastructure.Services
 {
     public class BucketOrderProcessorService
     {
+
+        MQTTPublisher mQTTPublisher;
         OrderStateMachineService orderStateMachineService;
         public BucketOrderProcessorService(OrderStateMachineService orderStateMachineService)
         {
@@ -20,7 +23,16 @@ namespace Infrastructure.Services
 
             string jsonString = JsonSerializer.Serialize(unsOrders);
 
+            var options = new HiveMQClientOptions();
+            options.Host = "bf271dc2c3614d1abd4a94cbe9bbdfc5.s1.eu.hivemq.cloud";
+            options.Port = 8883;
+            options.UserName = "mihai";
+            options.Password = "HiveMq2024";
+            options.UseTLS = true;
 
+            mQTTPublisher = new MQTTPublisher(options);
+            await mQTTPublisher.ConnectToBroker();
+            await mQTTPublisher.PublishMessage("Company/Europe/Romania/Cluj-Napoca/ERP/ProductionSchedule", jsonString);
 
             foreach (var unsOrder in unsOrders)
             {
